a8c552d97024ad39cb42a238b400ad3ab90dd64d
cmar <christopher.mar@gmail.com>
2013-03-02 09:21:59 -0500

    new rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rubyloco::Application.config.secret_token = '98242e1177f12b98eca590c3bb3bc6125fe822708a92715da09691e1beb1444c12c9f1e633a32f435b16456c67b7e8f16db5450027fbb4c1977827c7b56581fe'
